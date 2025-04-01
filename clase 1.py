049ef805dbd9588af58685076da8cafdc862727f
lienad85 <danielvillota@cca.edu.com>
2025-04-01 17:54:23 -0500

    mensaje

--- clase 1.py
+++ clase 1.py
@@ -1,1 +1,13 @@
-101010101
+ho = int(input("pon las horas trabajadas en la semana: "))
+ss = int(input("salario por semana: "))
+
+if ho == 40 and ss==100000:
+    ss + 0
+elif ho == 60 and ss==100000:
+    ss + 50000
+elif ho == 80 :
+    ss + 100000
+else:
+    print ("no se puede calcular")
+
+print (ss)
